080cf78373be362c8f38ff3facef5636ace04282
Farkhad Karim <kafa@Farkhads-MacBook-Air.local>
2025-01-28 23:39:58 +0500

    prjct started

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <header></header>
+
+  <main>
+    <h1>EMENTOR</h1>
+  </main>
+</template>
+
+<style scoped>
+header {
+  background-color: #dda1a1;
+}
+</style>
